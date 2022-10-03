887478ceac3301a5143939f7b9e4fcab79799551
IRojas44 <110552071+IRojas44@users.noreply.github.com>
2022-10-03 13:51:59 -0600

    Add files via upload

---
+++ Nodo.h
@@ -0,0 +1,40 @@
+//#include "../dispositivo/Dispositivo.h"
+#include <iostream>
+
+#ifndef NODO 
+
+#define NODO 1
+
+using namespace std;
+
+template <class T>
+class Node {
+    private:
+        T *data;
+        Node *next;
+    
+    public:
+        Node() {
+            data = NULL;
+            next = NULL;
+        }
+
+        Node(T *pData) {
+            this->data = pData;
+            next = NULL;
+        }
+
+        T* getData() {
+            return data;
+        }
+
+        Node* getNext() {
+            return next;
+        }
+
+        void setNext(Node *pValue) {
+            this->next = pValue;
+        }
+};
+
+#endif

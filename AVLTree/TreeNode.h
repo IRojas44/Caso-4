b178182dc2e16c0310322f11f9aa7a8178fe9b97
IRojas44 <110552071+IRojas44@users.noreply.github.com>
2022-10-22 21:06:10 -0600

    Arbol AVL

---
+++ AVLTree/TreeNode.h
@@ -0,0 +1,25 @@
+#include<iostream>
+
+#ifndef NODE
+
+#define NODE 1
+
+class TreeNode {
+  public:
+    int value;
+  TreeNode * left;
+  TreeNode * right;
+
+  TreeNode() {
+    value = 0;
+    left = NULL;
+    right = NULL;
+  }
+  TreeNode(int v) {
+    value = v;
+    left = NULL;
+    right = NULL;
+  }
+};
+
+#endif
